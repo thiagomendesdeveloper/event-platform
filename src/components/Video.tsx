03399d04525582ab032b756a94598cc6f21b3049
thiagomendesdeveloper <thiagomendessilva77@gmail.com>
2022-07-07 17:56:15 -0300

    video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,11 @@
 export function Video(){
     return(
         <div className="flex-1">
-            <h1>Video</h1>
+            <div className="bg-black">
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+
+                </div>
+            </div>
         </div>
     )
 }
